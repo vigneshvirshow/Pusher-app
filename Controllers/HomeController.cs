d8053e26084b1e822c6df021fa7eada75c10fb9e
Vignesh S <vignesh.sn@pitsolutions.com>
2024-06-11 12:35:03 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Pusher_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
